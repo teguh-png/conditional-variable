bbfcc0715d6a2c26daa755f44ad5d1ec22de8b4a
Teguh Firmansyah <firmansyahtgh13@gmail.com>
2022-11-20 23:26:43 +0800

    Revisi

--- script.js
+++ script.js
@@ -4,18 +4,21 @@ const listMahasiswa = [
     nim: 25,
     umur: 25,
     kelas: "A1",
+    tahunLulus: 2022
   },
   {
     nama: "M. Adam",
     nim: 40,
     umur: 24,
     kelas: "A2",
+    tahunLulus: 2020
   },
   {
     nama: "Andika",
     nim: 30,
     umur: 26,
     kelas: "A1",
+    tahunLulus : 2021
   },
 ];
 
@@ -25,8 +28,9 @@ console.log(namaMahasiswa);
 const mahasiswaA1 = listMahasiswa.filter((mahasiswa) => mahasiswa.kelas == "A1");
 console.log(mahasiswaA1);
 
-const kondisiAnd = listMahasiswa[0].nim < 35 && "Kelas A1";
-console.log(kondisiAnd);
 
-const kondisiOr = listMahasiswa[0].nim < 35 || "Kelas A2";
-console.log(kondisiOr);
+const kondisiAnd = listMahasiswa[0].tahunLulus >= 2022 && "Fresh Graduated"; //Jika kondisi true, akan mereturn "Fresh Graduate" 
+console.log(kondisiAnd); // Fresh Graduate
+
+const kondisiOr = listMahasiswa[1].tahunLulus >= 2022 || "Lulusan Lama"; //Jika kondisi false akan mereturn "Lulusan Lama"
+console.log(kondisiOr); // Lulusan Lama
